7769d2284ee273cb248974cee2d0b6ff93ba11a2
SpilledMySalt <94653739+SpilledMySalt@users.noreply.github.com>
2024-01-13 22:30:26 -0500

    Create boxes and center inside row
    Boxes are still not showing up, need to fix CSS for that.

--- etch.js
+++ etch.js
@@ -4,7 +4,7 @@ function createRow(num){
         const div = document.createElement("div");
         div.className = "boxRow";
         div.style.display = "flex";
-        div.style.flexDirection = "column";
+        div.style.justifyContent = "center";
         div.style.border = "10px";
         div.style.backgroundColor = "black";
         div.style.margin = "2px";
@@ -26,7 +26,8 @@ function createBox(num){
         div.style.backgroundColor = "pink";
         div.style.margin = "2px";
 
-        document.querySelectorAll("boxRow").appendChild(div);
+        // Use last child to select last created element of class
+        document.querySelector(".boxRow:last-child").appendChild(div);
     }
 
     // Add num boxes to the row
